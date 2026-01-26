21bd2844c9856aebc09c14e40423a18dbc0fc61b
Daniel Nachun <daniel.nachun@gmail.com>
2026-01-26 13:26:20 -0800

    fix build

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 # Create package archive and install globally
 npm pack --ignore-scripts
 npm install -ddd \
+    --no-bin-links \
     --global \
     --build-from-source \
     ${SRC_DIR}/${PKG_NAME}-${PKG_VERSION}.tgz
@@ -13,6 +14,13 @@ npm install -ddd \
 pnpm install
 pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
 
+mkdir -p ${PREFIX}/bin
+tee ${PREFIX}/bin/lockfile-lint << EOF
+#!/bin/sh
+exec \${CONDA_PREFIX}/lib/node_modules/lockfile-lint/bin/lockfile-lint.js "\$@"
+EOF
+chmod +x ${PREFIX}/bin/lockfile-lint
+
 tee ${PREFIX}/bin/lockfile-lint.cmd << EOF
-call %CONDA_PREFIX%\bin\node %CONDA_PREFIX%\bin\lockfile-lint %*
+call %CONDA_PREFIX%\bin\node %CONDA_PREFIX%\lib\node_modules\lockfile-lint\bin\lockfile-lint.js %*
 EOF
